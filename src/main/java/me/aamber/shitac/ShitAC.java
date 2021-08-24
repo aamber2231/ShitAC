010c149979af2a37f2b85a41c616b206e4fda365
aamber2231 <lolzs@had-sex.xyz>
2021-08-24 03:49:20 -0400

    Initial commit

---
+++ src/main/java/me/aamber/shitac/ShitAC.java
@@ -0,0 +1,17 @@
+package me.aamber.shitac;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShitAC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
